3f35e5ab42ee788e5d09633037b86ca6723e5b04
ValentineOO <varlomxg@gmail.com>
2023-10-23 15:52:17 +0100

    worked on

---
+++ src/pages/users/Users.tsx
@@ -0,0 +1,67 @@
+import DataTable from "../../components/dataTable/DataTable";
+import { userRows } from "../../data";
+import "./Users.scss";
+import { GridColDef } from "@mui/x-data-grid";
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 70 },
+  {
+    field: "img",
+    headerName: "Avatar",
+    width: 80,
+    renderCell: (params) => {
+      return <img src={params.row.img || "/noavatar.png"} alt="" />;
+    },
+  },
+
+  {
+    field: "firstName",
+    type: "string",
+    headerName: "First name",
+    width: 100,
+  },
+  {
+    field: "lastName",
+    type: "string",
+    headerName: "Last name",
+    width: 100,
+  },
+  {
+    field: "email",
+    type: "string",
+    headerName: "Email",
+    width: 160,
+  },
+  {
+    field: "phone",
+    type: "string",
+    headerName: "Phone",
+    width: 120,
+  },
+  {
+    field: "createdAt",
+    headerName: "Created At",
+    width: 100,
+    type: "string",
+  },
+  {
+    field: "verified",
+    headerName: "Verified",
+    width: 100,
+    type: "boolean",
+  },
+];
+
+const Users = () => {
+  return (
+    <div className="users">
+      <div className="info">
+        <h1>Users</h1>
+        <button>Add New Users</button>
+      </div>
+      <DataTable slug="users" columns={columns} rows={userRows} />
+    </div>
+  );
+};
+
+export default Users;
